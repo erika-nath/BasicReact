5b334ec33a3a7b5cef454b7afe0c9d52499b199a
Nath <enath6.25@gmail.com>
2021-12-25 23:07:58 -0600

    Ejemplo de JS imperativo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-function MainContect(){
-    return(
-        <h1>Esto es renderizar con una funcion</h1>
-    )
-};
-
-ReactDOM.render(<div> <MainContect /></div>, document.getElementById("root"));
+//React es declarativo a comparacion de JS que es imperativo
+//EJEMPLO EN JS 
+const h1 = document.createElement("h1")
+h1.textContent="Esto es un ejemplo en Vanilla que es imperativo, tambien son más lineas."
+h1.className = "header"
+document.getElementById("root").append(h1)
