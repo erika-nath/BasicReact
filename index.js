fb8be32860bd362882cd75c304f2bf4b27834323
Nath <enath6.25@gmail.com>
2021-12-25 23:16:27 -0600

    Declarativo con react,ejemplo

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 //React es declarativo a comparacion de JS que es imperativo
 //EJEMPLO EN JS 
-const h1 = document.createElement("h1")
+/*const h1 = document.createElement("h1")
 h1.textContent="Esto es un ejemplo en Vanilla que es imperativo, tambien son más lineas."
 h1.className = "header"
-document.getElementById("root").append(h1)
+document.getElementById("root").append(h1)*/
+
+//Ejemplo con REACT: se van a reducir las lineas de codigo**
+
+ReactDOM.render(<h1 className="header">Declarativo con React!!</h1>, document.getElementById("root"))
