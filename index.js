5bb3bb36ee4a27710411b00e762c7486956ed2ac
Nath <enath6.25@gmail.com>
2021-12-25 22:26:45 -0600

    Renderizar con una funcion

---
+++ index.js
@@ -0,0 +1,7 @@
+function MainContect(){
+    return(
+        <h1>Esto es renderizar con una funcion</h1>
+    )
+};
+
+ReactDOM.render(<div> <MainContect /></div>, document.getElementById("root"));
