92bbb8f8ce6d8fc0113e0ad0e939e05228002ba2
Nath <enath6.25@gmail.com>
2022-01-01 19:48:19 -0600

    Primer commit:Maquetando componentes que se van a utilizar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+//import './App.css';[
+
+import React from "react";
+
+//Primero se identifican los componentes que se van a utilizar
+// se identifica que elementos tendra cada componente y su funcionamiento
+
+//se crea una const con un array con los todos los cuales tienen sus propiedades
+const todos = [
+  { text:"Revisar correo", completed: false },
+  { text:"Ir al super", completed: false },
+  { text:"Agendar al dentista", completed: false },
+]
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+    {/*<TodoCounter />*/}
+    <h2>Has completoado 2 de 3 ToDo's</h2>
+    {/*<TodoSearch />*/}
+    <input placeholder="Cebolla" />
+    {/* <TodoList>{todos.map(todo =>(
+      <TodoItem />
+    ))}
+    </TodoList> */}
+    {/*<CreateTodoButton />*/}
+    <button>+</button>
+    </React.Fragment>
   );
 }
+// los {} son para escribir js en jsx
+//dentro de  TodoList se van a iterar los todos ,por cada elemento cada todo se va a renderizar
+// para comentar los commponentes se utiliza {/*....*/}
 
 export default App;
