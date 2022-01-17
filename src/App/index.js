311221c11f1814402c5c6bf5970bf693070424b0
erika-nath <enath6.25@gmail.com>
2022-01-16 23:35:13 -0600

    se agrega funcion de localStorage

--- src/App/index.js
+++ src/App/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { AppUI } from "./AppUI";
 
-const defaultTodos = [
+/*const defaultTodos = [
   { text:"Revisar correo", completed: false  },
   { text:"Ir al super", completed: true },
   { text:"Agendar al dentista", completed: false },
-]
+]*/
 
-
+//funcion para guaradr en local storage
 function App() {
-  const [todos, setTodos]= React.useState(defaultTodos);
-  const[searchValue, setSearchValue]= React.useState("");
+  //recuperar TODOs
+const localStorageTodos = localStorage.getItem('TODOS_V1')
+//variable guarda TODOS
+let parsedTodos;
+//Condicional si hay todos ó no hay
+if (!localStorageTodos){
+  localStorage.setItem("TODOS_V1",JSON.stringify([]));
+  parsedTodos = [];
+
+}else{
+  parsedTodos= JSON.parse(localStorageTodos);
+}
+
+const [todos, setTodos]= React.useState(parsedTodos);
+const[searchValue, setSearchValue]= React.useState("");
 // total de completadosy total de todos
 const completedTodos = todos.filter(todo=> todo.completed == true).length;
 const totalTodos = todos.length;
@@ -28,11 +41,20 @@ if(!searchValue.length >=1){
   });
 }
 
+//funcion que conecta localstorage con funcion de borrar y marcar TODOS
+const saveTodos = (newTodos) =>{
+  const stringifiedTodos = JSON.stringify(newTodos);
+  //stringifiedTodos es la nueva actualizacion de todos
+  localStorage.setItem('TODOS_V1',stringifiedTodos);
+  setTodos(newTodos);
+};
+
+
 const completeTodo = (text) => {
   const todoIndex = todos.findIndex(todo => todo.text === text);
-  const newTodos =[...todos];
+  const newTodos = [...todos];
   newTodos [todoIndex].completed = true;
-  setTodos(newTodos);
+  saveTodos(newTodos);
 };
 
 
@@ -40,7 +62,7 @@ const deleteTodo = (text) => {
   const todoIndex = todos.findIndex(todo => todo.text === text);
   const newTodos =[...todos];
   newTodos.splice(todoIndex, 1);
-  setTodos(newTodos);
+  saveTodos(newTodos);
 };
 
 
